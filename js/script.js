239a1c9fb17c8b9a9f89effbea0ddef9a03e45b7
victor <victor.fdez.fuentenebro11@gmail.com>
2026-02-01 18:23:01 +0100

    Aumentar zoom al hacer click en Europa

--- js/script.js
+++ js/script.js
@@ -270,8 +270,11 @@ function zoomToEurope() {
                 onEachFeature: onCountryEachFeature
             }).addTo(map);
 
-            // Zoom to Europe smoothly
-            map.flyToBounds(europeLayer.getBounds(), {
+            // Zoom to Europe smoothly (Focused on Central Europe with more zoom)
+            // Using flyTo instead of bounds to force a closer view than the default "fit all"
+            const zoomLevel = window.innerWidth < 768 ? 5 : 5; // Zoom 5 is good for both, maybe 4.5 for tiny screens
+
+            map.flyTo([48, 12], 5, {
                 duration: 3,
                 easeLinearity: 0.1
             });
